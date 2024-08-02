c12719d74de21694232077fc0f509f6b2af7f8d9
William_sanchez <sanchezalvarez12@hotmail.com>
2024-08-02 00:00:24 -0500

    Se actualiza comentario

--- src/apollo/apolloClient.jsx
+++ src/apollo/apolloClient.jsx
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { createHttpLink } from '@apollo/client/link/http';
 
-// Enlace HTTP a tu API de Pokémon
 const httpLink = createHttpLink({
     uri: 'https://beta.pokeapi.co/graphql/v1beta', // URL de la API de Pokémon con graphql
 });
